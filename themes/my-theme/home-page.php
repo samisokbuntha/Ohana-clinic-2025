eb552760beeeff2e91ce8b44de8d293adaee69c5
SAMI Sokbuntha <samisokbuntha@gmail.com>
2025-12-25 10:44:36 +0700

    test

--- themes/my-theme/home-page.php
+++ themes/my-theme/home-page.php
@@ -1,15 +1,1 @@
-<?php
-    get_home_page();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <h1>Home Page</h1>
-</body>
-</html>
+<h1>hello</h1>
