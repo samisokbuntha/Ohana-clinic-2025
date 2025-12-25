2e045bf086074a42518406fd36a01e1729d112d8
SAMI Sokbuntha <samisokbuntha@gmail.com>
2025-12-25 10:55:39 +0700

    header

---
+++ themes/my-theme/header.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<h1>All Products</h1>
+<div class="products">
+<?php while(have_posts()): the_post(); $price=get_post_meta(get_the_ID(),'_price',true); ?>
+<div class="product">
+<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+<h2><?php the_title(); ?></h2>
+<p>Price: <?php echo $price; ?></p>
+</div>
+<?php endwhile; ?>
+</div>
